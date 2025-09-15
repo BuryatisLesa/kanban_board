d95b7c6bee9f0c0d14920e7ec7e536f3eaef8001
Konstantin Sandanov <kostyansandanov@gmail.com>
2025-09-16 02:51:29 +0800

    feat: минимальная структура header закончена

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,12 +1,22 @@
+import styles from "./style.module.css"
+import userAvatar from "./media/user-avatar.png";
+import { JSX } from "react";
 
-function Header(){
+type TypeHeaderProps = {
+    title: string
+}
+
+function Header({title}: TypeHeaderProps): JSX.Element{
     return (
-        <header className="header">
-            <h1 className="header__title">Awesome Kanban Board</h1>
-            <div className="header__user-menu">
-                <img src="" alt="" className="header__user-menu-avatar" />
+        <header className={styles["header"]}>
+            <h1 className={styles["header__title"]}>{title}</h1>
+            <div className={styles["header__user-menu"]}>
+                <div className={styles["header__container-avatar"]}>
+                <img src={userAvatar} alt="" className={styles["header__user-menu-avatar"]} />
+                </div>
+                <button className={styles["header__open-menu"]}>
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g clip-path="url(#clip0_2_9)">
+                    <g clipPath="url(#clip0_2_9)">
                     <path d="M7.415 8.20999L12 12.795L16.585 8.20999L18 9.62499L12 15.625L6 9.62499L7.415 8.20999Z" fill="white"/>
                     </g>
                     <defs>
@@ -15,6 +25,7 @@ function Header(){
                     </clipPath>
                 </defs>
                 </svg>
+                </button>
             </div>
         </header>
     ) 
---
+++ src/Components/Main/index.jsx
@@ -0,0 +1,8 @@
+import styles from './style.module.css'
+
+function Main(){
+    return (
+        <main className={styles["main"]}>
+        </main>
+    )
+}
